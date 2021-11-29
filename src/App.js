fc6e7c3465a1907c13346556adb1c48273c3396e
crisboxing11 <crisboxing11@gmail.com>
2021-11-28 20:18:58 -0800

    search start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Accordion from './components/Accordion'
+import Search from './components/Search'
 
 const items = [
     {title:'What is React?',
@@ -16,6 +17,7 @@ export default () => {
     return (
         <div>
             <Accordion items={items} />
+            <Search />
             </div>
     )
 }
