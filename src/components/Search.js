a7f8459df2fbfaa18debb1fc76c333d39f3f5f90
crisboxing11 <crisboxing11@gmail.com>
2021-11-30 19:07:06 -0800

    fixed useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,8 +6,19 @@ const Search = () =>  {
 
     useEffect(() => {
        const search = async() => {
-           await axios.get{''}
-       }
+           await axios.get("https://en.wikipedia.org/w/api.php", {
+            params: {
+                action: 'query',
+                list: 'search',
+                origin: '*',
+                format: 'json',
+                srsearch: term
+                
+            },
+        });
+           };
+           search()
+       
     },[term]);
     return (
         <div>
