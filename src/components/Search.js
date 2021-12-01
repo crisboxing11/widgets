ba366d7b79ea2dcbcad4d7acd45f787b526d7afd
crisboxing11 <crisboxing11@gmail.com>
2021-11-30 18:56:26 -0800

    added axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,14 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 
 const Search = () =>  {
     const [term, setTerm] = useState('');
 
     useEffect(() => {
-       
-    },);
+       const search = async() => {
+           await axios.get{''}
+       }
+    },[term]);
     return (
         <div>
             <div className='ui form'>
