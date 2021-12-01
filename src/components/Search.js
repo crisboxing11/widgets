8b10162763a75a40a19be8224c0c83237515df3c
crisboxing11 <crisboxing11@gmail.com>
2021-11-30 19:26:04 -0800

    added default state

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,24 +2,27 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
 const Search = () =>  {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('Programming');
     const [results, setResults] = useState([])
+console.log(results)
 
     useEffect(() => {
-       const search = async() => {
-           const { data }= await axios.get("https://en.wikipedia.org/w/api.php", {
+       const search = async () => {
+           const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
             params: {
                 action: 'query',
                 list: 'search',
                 origin: '*',
                 format: 'json',
-                srsearch: terms
+                srsearch: term,
             },
         });
+        setResults(data)
            };
-
-           setResults(data)
-       
+           
+            search();
+           
+           
     },[term]);
     return (
         <div>
