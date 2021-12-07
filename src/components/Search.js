48ecce51c3f9816e977b28930bab4fd7b8aeb90e
crisboxing11 <crisboxing11@gmail.com>
2021-12-06 20:03:32 -0800

    added routes

--- src/components/Search.js
+++ src/components/Search.js
@@ -26,7 +26,11 @@ const Search = () =>  {
         return (
         <div key={results.pageid} className ="item">
             <div className="right floated content">
-                <a className="ui button">Go</a>
+                <a 
+                className="ui button"
+                href={`https://en.wikipedia.org?curid=${results.pageid}`}
+                >Go
+                </a>
             </div>
             <div className ="content">
                 <div className="header">
