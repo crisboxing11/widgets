ff4fef17a08c397623996e79584131d82804acde
crisboxing11 <crisboxing11@gmail.com>
2021-12-11 15:30:28 -0800

    Props added

--- src/App.js
+++ src/App.js
@@ -14,10 +14,24 @@ content: 'React is a favorite JS library among engineers'
 content:'You use React by creating components'}
 ]
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: "The Color Green",
+        value: 'green'
+    },
+    {
+        label: "A Shade of Blue",
+        value: 'blue'
+    }
+];
 export default () => {
     return (
         <div>
-            <Dropdown />
+            <Dropdown options={options} />
         </div>
     )
 }
