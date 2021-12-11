f632ecf5acc9453196e75a0197b3ae544b9eeb6c
crisboxing11 <crisboxing11@gmail.com>
2021-12-11 15:26:15 -0800

    Starting on Dropdown Widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 
 const items = [
     {title:'What is React?',
@@ -16,7 +17,7 @@ content:'You use React by creating components'}
 export default () => {
     return (
         <div>
-            <Search />
-            </div>
+            <Dropdown />
+        </div>
     )
 }
