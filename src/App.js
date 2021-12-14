03c7da398e811f00de78cdcbcdc8844caccd71f1
crisboxing11 <crisboxing11@gmail.com>
2021-12-13 20:58:56 -0800

    passed useState as prop

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default () => {
     const [selected,setSelected] = useState(options[0])
     return (
         <div>
-            <Dropdown options={options} />
+            <Dropdown selected={selected} options={options} />
         </div>
     )
 }
