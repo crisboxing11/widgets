3889ca05cc93c9fef0caec4b99eff3ce315d3659
crisboxing11 <crisboxing11@gmail.com>
2021-12-18 12:41:38 -0800

    dropdown works now but requires a ref

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
 
-  const [toggle, setToggle] = useState(false);
+  const [open, setOpen] = useState(false);
 
 const renderedOptions = options.map((options) => {
   if (options.value === selected.value ) {
@@ -22,10 +22,10 @@ const renderedOptions = options.map((options) => {
       <div className="ui form">
         <div className="field">
           <label className="label">Select a Color</label>
-          <div className="ui selection dropdown visible active">
+          <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''} `}>
             <i className="dropdown icon"></i>
             <div className="text">{selected.label}</div>
-            <div className="menu visible transition">
+            <div className={`menu ${open ?'visible transition' : ''}`}>
                 {renderedOptions}
             </div>
           </div>
