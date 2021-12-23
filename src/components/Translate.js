1ab6fabcbac7e1483c688c50359027a1e66d285d
crisboxing11 <crisboxing11@gmail.com>
2021-12-23 15:15:57 -0800

    Label added to text input

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -20,11 +20,22 @@ const Translate = () => {
     const [language,setLanguage] = useState(options[0]);
     const [text,setText] = useState("");
     return (
-        <div>
-            <input value={text} onChange={(e) => setText(e.target.value)}   />
-            <Dropdown label="Select a Language" selected={language} onSelectedChange={setLanguage} options={options}/>
+      <div>
+        <div className="ui form">
+          <div className="field">
+              <label>Enter Text</label>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
+          </div>
         </div>
-    )
+
+        <Dropdown
+          label="Select a Language"
+          selected={language}
+          onSelectedChange={setLanguage}
+          options={options}
+        />
+      </div>
+    );
 }
 
 export default Translate
