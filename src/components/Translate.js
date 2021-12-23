73bc8530451bd2b2dfe7de76ecc36035365f40c8
crisboxing11 <crisboxing11@gmail.com>
2021-12-23 15:09:25 -0800

    Changed Label

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -20,7 +20,7 @@ const Translate = () => {
     const [language,setLanguage] = useState(options[0]);
     return (
         <div>
-            <Dropdown selected={language} onSelectedChange={setLanguage} options={options}/>
+            <Dropdown label="Select a Language" selected={language} onSelectedChange={setLanguage} options={options}/>
         </div>
     )
 }
