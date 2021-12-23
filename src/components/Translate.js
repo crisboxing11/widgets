aa8445dcff3ade9df42127999f80a02d531a450d
crisboxing11 <crisboxing11@gmail.com>
2021-12-23 14:54:52 -0800

    translate structure

---
+++ src/components/Translate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dropdown from './Dropdown';
+
+const Translate = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Translate
