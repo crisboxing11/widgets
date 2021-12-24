8a9c065ad324967ec59d40a31a72a5305dd819d3
crisboxing11 <crisboxing11@gmail.com>
2021-12-23 19:33:21 -0800

    Creating Convert Component

---
+++ src/components/Convert.js
@@ -0,0 +1,12 @@
+import React , { useState, useEffect, useRef }from 'react';
+import Translate from './Translate'
+
+const Convert = (text,language) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default 'Convert'
