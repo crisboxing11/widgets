6df484fd64bcee7e87325a6cd8dab71ab1a7db05
crisboxing11 <crisboxing11@gmail.com>
2022-01-07 15:48:05 -0800

    structure of header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
-    )
+      <div className="ui secondary pointing menu">
+        <a href="/" className="item">
+          Accordion
+        </a>
+        <a href="/list" className="item">
+          Search
+        </a>
+        <a href="/dropdown" className="item">
+          Dropdown
+        </a>
+        <a href="/translate" className="item">
+            Translate
+        </a>
+      </div>
+    );
 }
 
 export default Header
