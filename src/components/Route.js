e377928f12875a7142214ec22b6be8fc5dfb50cd
crisboxing11 <crisboxing11@gmail.com>
2022-01-07 15:18:25 -0800

    adding routes

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Route = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+const Route = ({ path,children }) => {
+   return window.location.pathname === path 
+   ? children
+   : null
 }
 
 export default Route
