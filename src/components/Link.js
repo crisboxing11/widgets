d90e963536137805ce7c52f5789559bb9c4a65fe
crisboxing11 <crisboxing11@gmail.com>
2022-01-08 23:04:53 -0800

    Stopping all extra request

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const Link = ({ className, href, children })  => {
+    const onClick = (e) => {
+        e.preventDefault()
+    }
     return (
         <div>
-            <a className={className} href={href}>{children}</a>
+            <a
+             className={className} 
+             href={href}
+             onClick={onClick}
+             >{children}</a>
         </div>
     )
 }
