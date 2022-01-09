0331f078e6d744326d6a69dc9e4a86fa5778a7d8
crisboxing11 <crisboxing11@gmail.com>
2022-01-08 23:17:13 -0800

    updated routing

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,16 @@
-
+import { useEffect } from 'react'
 const Route = ({ path,children }) => {
+    useEffect(() => {
+        const onLocationChange = () => {
+            console.log("location has changed")
+        };
+
+        window.addEventListener('popstate',onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange)
+        }
+    },[])
    return window.location.pathname === path 
    ? children
    : null
