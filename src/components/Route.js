49cb90f6a978a397177b78e5ab0ce427eae93a03
crisboxing11 <crisboxing11@gmail.com>
2022-01-08 23:19:40 -0800

    added useState to route component

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,7 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 const Route = ({ path,children }) => {
+const [currentPath,setCurrentPath] = useState(window.location.pathname)
+
     useEffect(() => {
         const onLocationChange = () => {
             console.log("location has changed")
